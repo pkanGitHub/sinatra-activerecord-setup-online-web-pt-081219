afbe919dfb30aa00e4f37cd0daa40e2ab8e1af70
Phyllis Kan <Phyllis Kan>
2019-11-29 03:39:50 -0600

    Done.

---
+++ db/migrate/20191129093333_create_dogs.rb
@@ -0,0 +1,21 @@
+class CreateDogs < ActiveRecord::Migration[5.2]
+  def up
+    create_table :dogs do |t|
+      t.string :name
+      t.string :breed
+    end
+  end
+
+  def down
+    drop_table :dogs
+  end
+
+# if not use up and down, use change will be like this
+#   def change 
+#     create_table :dogs do |t|
+#       t.string :name
+#       t.string :breed
+#     end
+#   end
+
+end
